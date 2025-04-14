4b47bc34f25ebef8e63ec9ff3da2c06e25a0eea2
Vaishnavi02k <85024452+Vaishnavi02k@users.noreply.github.com>
2025-04-14 20:35:03 +0530

    update firebase-config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,10 +4,10 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcCa2-jVQLDQlSuXZeFAuswyNx_c_N7b4",
-  authDomain: "hostel-mess-8ba8a.firebaseapp.com",
-  projectId: "hostel-mess-8ba8a",
-  storageBucket: "hostel-mess-8ba8a.firebasestorage.app",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: "1088097622877",
   appId: "1:1088097622877:web:7c10afbe3248d7118fe6b4",
   measurementId: "G-0P9M8T426Z"
