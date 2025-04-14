cfc748b323f7dd4d4397fd8493930281442b10ae
Vaishnavi02k <85024452+Vaishnavi02k@users.noreply.github.com>
2025-04-14 21:53:17 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from '.';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Layout from './Layout';
 import Dashboard from './components/Dashboard/Dashboard';
